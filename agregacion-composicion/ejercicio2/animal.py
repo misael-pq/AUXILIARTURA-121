d697ab530f4e78d3654ca2778a1f215ad546ac5b
misael-pq <misaelportillo1470@gmail.com>
2026-04-26 02:07:32 -0400

    Delete agregacion-composicion directory

--- agregacion-composicion/ejercicio2/animal.py
+++
@@ -1,5 +0,0 @@
-class Animal:
-    def __init__(self, nombre, edad, nombreDueno):
-        self.nombre = nombre
-        self.edad = edad
-        self.nombreDueno = nombreDueno
--- agregacion-composicion/ejercicio2/gato.py
+++
@@ -1,7 +0,0 @@
-from animal import Animal
-
-class Gato(Animal):
-    def __init__(self, nombre, edad, nombreDueno, cazaRatones, tomaLeche):
-        super().__init__(nombre, edad, nombreDueno)
-        self.cazaRatones = cazaRatones
-        self.tomaLeche = tomaLeche
